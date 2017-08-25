50699d9f18e2e5b3a5e877651dd4b39c59d84038
James Rising <jarising@gmail.com>
2017-08-25 15:47:49 -0500

    include all algorithms

--- test/test_uncertainty2.jl
+++ test/test_uncertainty2.jl
@@ -48,25 +48,35 @@ run(m)
 import OptiMimi.uncertainproblem
 using DataFrames
 
-df = DataFrame(mcperlife=[], cons1=[], cons2=[], cons3=[], cons4=[], cons5=[], cons6=[], cons7=[], cons8=[], cons9=[], cons10=[], cons1serr=[], cons2serr=[], cons3serr=[], cons4serr=[], cons5serr=[], cons6serr=[], cons7serr=[], cons8serr=[], cons9serr=[], cons10serr=[])
-push!(df, [Inf; repmat([1/3], 10); repmat([0], 10)])
+df = DataFrame(algorithm=[], mcperlife=[], cons1=[], cons2=[], cons3=[], cons4=[], cons5=[], cons6=[], cons7=[], cons8=[], cons9=[], cons10=[], cons1serr=[], cons2serr=[], cons3serr=[], cons4serr=[], cons5serr=[], cons6serr=[], cons7serr=[], cons8serr=[], cons9serr=[], cons10serr=[], time=[])
+push!(df, [:exact; Inf; repmat([1/3], 10); repmat([0], 10); 0])
 
-for mcperlife in 1:2:10
-    println(mcperlife)
-    prob = uncertainproblem(m, [:Bellmano], [:consumption], [0.], [1.], m -> sum(m[:Bellmano, :utility] .* exp(-(0:9) * .05)), (model) -> nothing)
-    minfmean, minfserr, minxmean, minxserr = solution(prob, () -> repmat([.25], 10), mcperlife, 100)
-    push!(df, [mcperlife; minxmean; minxserr])
-    println(df)
+for samplemult in [1, 2]
+    for mcperlife in [5, 10, 50]
+        println(mcperlife)
+        prob = uncertainproblem(m, [:Bellmano], [:consumption], [0.], [1.], m -> sum(m[:Bellmano, :utility] .* exp(-(0:9) * .05)), (model) -> nothing)
+
+        tic()
+        soln = solution(prob, () -> repmat([.25], 10), :social, mcperlife, 16*samplemult)
+        time = toc()
+        push!(df, [:social; mcperlife; soln.xmean; soln.xserr; time])
+
+        println(df)
+
+        tic()
+        soln = solution(prob, () -> repmat([.25], 10), :biological, mcperlife, 10000*samplemult)
+        time = toc()
+        push!(df, [:biological; mcperlife; soln.xmean; soln.xserr; time])
+
+        println(df)
+
+        tic()
+        soln = solution(prob, () -> repmat([.25], 10), :sampled, mcperlife, 100*samplemult)
+        time = toc()
+        push!(df, [:sampled; mcperlife; soln.xmean; soln.xserr; time])
+    
+        println(df)
+    end
 end
 
-writetable("attempts-nonstoch.csv", df)
-
-for mcperlife in 15:20:100
-    println(mcperlife)
-    prob = uncertainproblem(m, [:Bellmano], [:consumption], [0.], [1.], m -> sum(m[:Bellmano, :utility] .* exp(-(0:9) * .05)), (model) -> nothing)
-    minfmean, minfserr, minxmean, minxserr = solution(prob, () -> repmat([.25], 10), mcperlife, 100)
-    push!(df, [mcperlife; minxmean; minxserr])
-    println(df)
-end
-
-writetable("attempts-nonstoch.csv", df)
+writetable("attempts-compare.csv", df)
