92f309df66e93bb427bda30a70c4d171768edf06
James Rising <jarising@gmail.com>
2019-03-24 09:08:40 +0000

    more fixes

--- test/test_dupover.jl
+++ test/test_dupover.jl
@@ -13,9 +13,10 @@ set_dimension!(m, :region, collect(1:2))
 set_dimension!(m, :time, collect(1:3))
 
 simple = add_comp!(m, Simple)
-simple[:xx] = reshape(repeat([1, 2], inner=3), (3, 2))
+xx = reshape(repeat([1, 2], inner=3), (3, 2))
+simple[:xx] = xx
 
-gen(rr) = m.external_parameters[:xx].values[rr, 1]
+gen(rr) = xx[rr, 1]
 hall = hallsingle(m, :Simple, :xx, gen, [:time])
 
 @test reshape(hall.f, (3, 2)) == m.external_parameters[:xx].values
