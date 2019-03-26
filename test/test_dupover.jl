ff8d40c75ac143dbfd264c4c538e862267c1d2a6
James Rising <jarising@gmail.com>
2019-03-26 16:16:40 +0000

    new syntax

--- test/test_dupover.jl
+++ test/test_dupover.jl
@@ -15,7 +15,7 @@ set_dimension!(m, :time, collect(1:3))
 simple = add_comp!(m, Simple)
 simple[:xx] = reshape(repeat([1, 2], inner=3), (3, 2))
 
-gen(rr) = m.external_parameters[:xx].values[rr, 1]
+gen(rr) = m.md.external_params[:xx].values[rr, 1]
 hall = hallsingle(m, :Simple, :xx, gen, [:time])
 
-@test reshape(hall.f, (3, 2)) == m.external_parameters[:xx].values
+@test reshape(hall.f, (3, 2)) == m.md.external_params[:xx].values
